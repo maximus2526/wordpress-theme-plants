6dcad86f39b3b35321db89be8eee16ae06f51dcf
Maxim Kliakhin <m.kliakhin@gmail.com>
2023-09-01 15:11:41 +0300

    +

--- sidebar.php
+++ sidebar.php
@@ -10,10 +10,34 @@
 
 if ( is_page() ) :
 	?>
-<div class="container">
-	<?php
-	echo '================sidebar================';
-	?>
-</div>
+<div id="primary" class="sidebar">
+
+	<?php do_action( 'before_sidebar' ); ?>
+
+	<?php if ( ! dynamic_sidebar( 'sidebar-primary' ) ) : ?>
+
+		<aside id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</aside><!-- #search -->
+
+		<aside id="archives" class"widget">
+			<h3 class="widget-title"><?php echo __( 'Archives', 'plants' ); ?></h3>
+			<ul>
+				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		</aside><!-- #archives -->
+
+		<aside id="meta" class="widget">
+			<h3 class="widget-title"><?php _e( 'Meta', 'plants' ); ?></h3>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</aside><!-- #meta -->
+
+	<?php endif; ?>
+
+</div><!-- #primary -->
 	<?php
 endif;
