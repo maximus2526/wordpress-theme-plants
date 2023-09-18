048c57418de4ed3fef90f4bdb67a1304e73cc9a7
Maxim Kliakhin <m.kliakhin@gmail.com>
2023-09-18 10:02:05 +0300

    Attachment bug fixed

--- assets/js/plants_admin.js
+++ assets/js/plants_admin.js
@@ -2,23 +2,33 @@
 (function ($) {
 	function openEditorFunc() {
 		$('.upload_image_button').on("click", (function () {
-			let frame;
+			let $frame;
 			$button = $(this);
-			frame = wp.media({
+			$frame = wp.media({
 				title: 'Select or Upload Media For Menu Items',
 				button: {
 					text: 'Use this media'
-			},
-				multiple: false 
+				},
+				multiple: false
 			});
 
-   frame.open();
-   
-			frame.on( 'select', function() {
-				let $attachment = frame.state().get('selection').first().toJSON();
-				$button.parent().find('input[type="hidden"]').val( $attachment.id );;
-				$button.parent().find('img').removeAttr('srcset').attr('src',  $attachment.url);
-		});
+			$frame.open();
+
+			$frame.on('select', function () {
+				let $attachment = $frame.state().get('selection').first().toJSON();
+				let $attachmentImg = $button.parent().find('img');
+				$button.parent().find('input[type="hidden"]').val($attachment.id);
+				if ( $attachmentImg.length > 0 ) {
+					$attachmentImg.removeAttr('srcset').attr('src', $attachment.url);
+				} else {
+					let $image = new Image();
+					$image.height = 50;
+					$image.width = 50;
+					$image.src = $attachment.url;
+					$button.parent().prepend($image).show();
+					$button.parent().find('.remove_image_button').show();
+				}
+			});
 			return false;
 		}));
 	}
@@ -27,17 +37,20 @@
 
 	function removeImageFunc() {
 		$('.remove_image_button').on("click", (function () {
-			let r = confirm("You sure?");
-			var thisPointer = $(this);
-			if (r == true) {
-				thisPointer.parent().find('input[type="hidden"]').val('');
-				thisPointer.hide();
-				thisPointer.parent().find('img').hide();
+			let $r = confirm("You sure?");
+			var $thisPointer = $(this);
+			if ($r == true) {
+				$thisPointer.parent().find('input[type="hidden"]').val('');
+				$thisPointer.hide();
+				$thisPointer.parent().find('img').remove();
 			}
-
 			return false;
 		}));
+		
 	}
+	
+		
+
 
 	$(document).ready(function () {
 		openEditorFunc();
